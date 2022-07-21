75423e9e6a118fcd3dd666896f97d607cf0bf86c
hoanprd <tinhprd@gmail.com>
2022-07-21 12:07:55 +0700

    first commit

---
+++ FPS Denblyn 3D Game/Assets/Scripts/Weapons/HandGunPickUp.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandGunPickUp : MonoBehaviour
+{
+    public GameObject fakeHandGun;
+    public GameObject realHandGun;
+    public GameObject Message;
+    public AudioSource handGunPickUpSound;
+    public bool IsPickUp = false;
+
+    void Update()
+    {
+        if(IsPickUp == true && Input.GetKeyDown(KeyCode.F))
+        {
+            fakeHandGun.SetActive(false);
+            realHandGun.SetActive(true);
+            handGunPickUpSound.Play();
+            GetComponent<BoxCollider>().enabled = false;
+            Message.SetActive(false);
+            IsPickUp = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IsPickUp = true;
+        Message.SetActive(true);
+        Message.GetComponent<Text>().text = "Press 'F' to pick up";
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        IsPickUp = false;
+        Message.SetActive(false);
+    }
+}
